d25c203332c1b399a4364ba1a204379a01616c74
Dcrown <aisimiyuoluwadara@gmail.com>
2022-03-17 22:33:54 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,39 +1,20 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_line - check for a digit
- *
- *   * @n : number of _ to be printed
- *
- *    * Return:void
- *
- *     */
-
-
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
 
 void print_line(int n)
-
 {
 
+	int i = 0;
 
-
-		int i = 0;
-
-
-
-			while (i < n && n > 0)
-
-					{
-
-								_putchar('_');
-
-										i++;
-
-											}
-
-				_putchar('\n');
-
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
 }
