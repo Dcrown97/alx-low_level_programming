cac76f2144525c555f8fc39546f515bb88175bc8
Dcrown <aisimiyuoluwadara@gmail.com>
2022-04-12 14:08:00 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,151 +1,76 @@
 #include "dog.h"
 
-
-
 /**
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
  *
- *  * _strdup - create a new array containing a copy of the given string
- *
- *   * @str: a pointer to the string to copy
- *
- *    *
- *
- *     * Return: NULL if str is NULL or if memory allocation fails,
- *
- *      * otherwise a return a pointer to the new copy
- *
- *       */
-
+ * Return: NULL if str is NULL or if memory allocation fails,
+ * otherwise a return a pointer to the new copy
+ */
 char *_strdup(char *str)
-
 {
+	char *dup;
+	unsigned int size = 0;
 
-		char *dup;
+	if (str)
+	{
+		while (str[size++])
+					;
 
-			unsigned int size = 0;
+		dup = malloc(sizeof(char) * size);		
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
 
-
-
-				if (str)
-
-						{
-
-									while (str[size++])
-
-															;
-
-
-
-											dup = malloc(sizeof(char) * size);		
-
-													if (dup)
-
-																{
-
-																				while (size--)
-
-																									dup[size] = str[size];
-
-
-
-																							return (dup);
-
-																									}
-
-														}
-
-					return (NULL);
-
+			return (dup);
+		}
+	}
+	return (NULL);
 }
 
-
-
 /**
+ * new_dog - create a new dog
+ * @name: the new dog's name
+ * @age: the new dog's age
+ * @owner: the new dog's owner
  *
- *  * new_dog - create a new dog
- *
- *   * @name: the new dog's name
- *
- *    * @age: the new dog's age
- *
- *     * @owner: the new dog's owner
- *
- *      *
- *
- *       * Return: a pointer to the new dog, or NULL if memory allocation fails
- *
- *        */
-
+ * Return: a pointer to the new dog, or NULL if memory allocation fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
+	dog_t *d;
 
-		dog_t *d;
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
 
+	if (name)
+	{
+		d->name = _strdup(name);
+		if (!(d->name))
+		{
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->name = NULL;
 
+	d->age = age;
 
-			d = malloc(sizeof(dog_t));
+	if (owner)
+	{
+		d->owner = _strdup(owner);
+		if (!(d->owner))
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->owner = NULL;
 
-				if (!d)
-
-							return (NULL);
-
-
-
-					if (name)
-
-							{
-
-										d->name = _strdup(name);
-
-												if (!(d->name))
-
-															{
-
-																			free(d);
-
-																						return (NULL);
-
-																								}
-
-													}
-
-						else
-
-									d->name = NULL;
-
-
-
-							d->age = age;
-
-
-
-								if (owner)
-
-										{
-
-													d->owner = _strdup(owner);
-
-															if (!(d->owner))
-
-																		{
-
-																						free(d->name);
-
-																									free(d);
-
-																												return (NULL);
-
-																														}
-
-																}
-
-									else
-
-												d->owner = NULL;
-
-
-
-										return (d);
-
+	return (d);
 }
