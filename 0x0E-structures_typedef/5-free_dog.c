fbd9a00d1005f65c9c75f36d6d3380025d3d1218
Dcrown <aisimiyuoluwadara@gmail.com>
2022-04-12 14:09:54 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,65 +1,17 @@
 #include "dog.h"
 
-
-
 /**
+ * free_dog - free a dog
+ * @d: a pointer to the dog to free
  *
- *  * free_dog - free a dog
- *
- *   * @d: a pointer to the dog to free
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
+ * Return: void
+ */
 void free_dog(dog_t *d)
-
 {
-
-		if (d)
-
-				{
-
-							free(d->name);
-
-									free(d->owner);
-
-											free(d);
-
-												}
-
-}#include "dog.h"
-
-
-
-/**
- *
- *  * free_dog - free a dog
- *
- *   * @d: a pointer to the dog to free
- *
- *    *
- *
- *     * Return: void
- *
- *      */
-
-void free_dog(dog_t *d)
-
-{
-
-		if (d)
-
-				{
-
-							free(d->name);
-
-									free(d->owner);
-
-											free(d);
-
-												}
-
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
