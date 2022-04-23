f480e42b7a4bfd6a1b8b848066660b10ff959311
Dcrown <aisimiyuoluwadara@gmail.com>
2022-04-23 11:51:37 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,41 +1,21 @@
 #include "lists.h"
 
-
-
 /**
- *
- *  * free_list - frees a list_t list
- *
- *   * @head: head of linked list
- *
- *    */
-
+ * free_list - frees a list_t list
+ * @head: head of linked list
+ */
 void free_list(list_t *head)
-
 {
+	list_t *current;
+	list_t *nxt;
 
-		list_t *current;
+	current = head;
 
-			list_t *nxt;
-
-
-
-				current = head;
-
-
-
-					while (current != NULL)
-
-							{
-
-										nxt = current->next;
-
-												free(current->str);
-
-														free(current);
-
-																current = nxt;
-
-																	}
-
+	while (current != NULL)
+	{
+		nxt = current->next;
+		free(current->str);
+		free(current);
+		current = nxt;
+	}
 }
