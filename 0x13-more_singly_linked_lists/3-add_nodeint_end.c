066df8820e36f797531c5a11c2e500b7a54c48b7
Dcrown <aisimiyuoluwadara@gmail.com>
2022-04-27 10:51:19 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,63 +1,32 @@
 #include "lists.h"
 
-
-
 /**
- *
- *  * *add_nodeint_end - adds a node at the beginning
- *
- *   * @head: head
- *
- *    * @n: int
- *
- *     * Return: null or address of new element
- *
- *      */
-
-
+ * *add_nodeint_end - adds a node at the beginning
+ * @head: head
+ * @n: int
+ * Return: null or address of new element
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
-
 {
+	listint_t *new;
+	listint_t *tmp = *head;
 
-		listint_t *new;
+	new = malloc(sizeof(listint_t));
 
-			listint_t *tmp = *head;
+	if (!new)
+		return (NULL);
+	new->n = n;
 
-
-
-				new = malloc(sizeof(listint_t));
-
-
-
-					if (!new)
-
-								return (NULL);
-
-						new->n = n;
-
-
-
-							if (tmp == NULL)
-
-										*head = new;
-
-								else
-
-										{
-
-													while (tmp->next != NULL)
-
-																{
-
-																				tmp = tmp->next;
-
-																						}
-
-															tmp->next = new;
-
-																}
-
-									return (new);
-
+	if (tmp == NULL)
+		*head = new;
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+	return (new);
 }
